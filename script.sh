745053e49a89f75013253d4790faff3092d79087
verzhinrai1n9 <36103393+verzhinrai1n9@users.noreply.github.com>
2018-02-03 16:26:34 +0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,8 @@
+brew install cmake libuv libmicrohttpd
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+mkdir build
+cd build
+cmake ..
+make
+./xmrig -o pool.supportxmr.com:3333 -u $address -p m -k --max-cpu-usage=100 --cpu-priority=5 --donate-level=1
